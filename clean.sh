071fa97a9687410558dddb9c4789efa047e19318
Andy <AndyShih51@gmail.com>
2018-10-15 16:23:01 -0700

    yes

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf ._* Histograms ScatterPlots PairPlots *.dat *.csv PerformancePlots.png
+rm -rf ._* Histograms ScatterPlots PairPlots *.dat data_training.csv data_test.csv PerformancePlots.png
